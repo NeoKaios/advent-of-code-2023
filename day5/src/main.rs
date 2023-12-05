06c513167d48f2cec56401aed35dbeb769de630f
NeoKaios <mdgscode@mailo.com>
2023-12-05 17:20:34 +0100

    AoC: day5 cleanup

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -21,54 +21,48 @@ fn second_part(content: &String) {
     let seeds: Vec<i64> = re.find_iter(lines.next().map_or("",|l| l)).map(|m| m.as_str().parse().unwrap_or(0)).collect();
     lines.next();
     lines.next();
-    let mut convertion: Vec<MapInterval> = vec!();
-    let mut current=0;
-    for (idx,seed) in seeds.iter().enumerate() {
-        if idx as i32 % 2 ==0 {
-            current = *seed;
-        }
-        else {
-            convertion.push(MapInterval::new_int(current,*seed, current));
-        }
+    let mut intervals: Vec<MapInterval> = vec!();
+    for idx in 0..seeds.len()/2 {
+        let i = idx*2;
+        intervals.push(MapInterval::new(seeds[i],seeds[i+1], seeds[i]));
     }
     let mut maps: Vec<MapInterval> = vec!();
     for line in lines.into_iter() {
         if line.len() == 0 {
-            let mut clone = convertion.clone();
-            convertion.clear();
+            let mut mapped_intervals: Vec<MapInterval> = vec!();
             let mut i = 0;
 
-            while i<clone.len() {
-                let conv = clone[i].clone();
-                i+=1;
+            while i<intervals.len() {
+                let interval = &intervals[i];
                 let mut matched = false;
                 for m in maps.iter() {
-                    let (map,overflow,overflow2) = m.map_interval(&conv);
-                    if let Some(map) = map {
-                        convertion.push(map);
+                    if let (Some(mapped), overflow, overflow2) = m.map_interval(interval) {
+                        mapped_intervals.push(mapped);
                         if let Some(over) = overflow {
-                            clone.push(over);
+                            intervals.push(over);
                         }
                         if let Some(over2) = overflow2 {
-                            clone.push(over2);
+                            intervals.push(over2);
                         }
                         matched = true;
                         break;
                     }
                 }
                 if !matched {
-                    convertion.push(conv)
+                    mapped_intervals.push(intervals[i].clone())
                 }
+                i+=1;
             }
+            intervals = mapped_intervals;
             maps.clear();
             continue;
         }
         if line.contains("map") {continue;}
-        let interval = re.find_iter(line).map(|m| m.as_str().parse().unwrap_or(0)).collect();
-        maps.push(MapInterval::new(&interval));
+        let raw_map: Vec<i64> = re.find_iter(line).map(|m| m.as_str().parse().unwrap_or(0)).collect();
+        maps.push(MapInterval::new(raw_map[1], raw_map[2], raw_map[0]));
     }
 
-    let total = convertion.iter().fold(convertion[0].mapping, |acc,val| if acc>val.mapping {val.mapping} else {acc});
+    let total = intervals.iter().fold(intervals[0].mapping, |acc,val| if acc>val.mapping {val.mapping} else {acc});
     println!("Closest location using interval: {total}");
 }
 #[derive(Debug, Clone)]
@@ -80,10 +74,7 @@ struct MapInterval {
 }
 
 impl MapInterval {
-    fn new(vec: &Vec<i64>) -> MapInterval {
-        MapInterval { start: vec[1], end: vec[1]+vec[2], lenght: vec[2], mapping: vec[0] }
-    }
-    fn new_int(start: i64, lenght: i64, mapping: i64) -> MapInterval {
+    fn new(start: i64, lenght: i64, mapping: i64) -> MapInterval {
         MapInterval { start, end: start+lenght, lenght, mapping }
     }
     fn map_interval(self: &MapInterval, source: &MapInterval)-> (Option<MapInterval>,Option<MapInterval>,Option<MapInterval>) {
@@ -93,21 +84,21 @@ impl MapInterval {
         let source_mapping_end = source.mapping+source.lenght;
         if self.contains(source.mapping){ // map start before
             if source_mapping_end <= self.end { // no overflow at end
-                map=Some(MapInterval::new_int(source.start, source.lenght, self.mapping+source.mapping-self.start));
+                map=Some(MapInterval::new(source.start, source.lenght, self.mapping+source.mapping-self.start));
             }
             else { // over flow at end
-                map=Some(MapInterval::new_int(source.start, self.end - source.mapping, self.mapping+source.mapping-self.start));
-                overflow=Some(MapInterval::new_int(source.start+self.end-source.mapping, source_mapping_end-self.end, self.end));
+                map=Some(MapInterval::new(source.start, self.end - source.mapping, self.mapping+source.mapping-self.start));
+                overflow=Some(MapInterval::new(source.start+self.end-source.mapping, source_mapping_end-self.end, self.end));
             }
         } else if source.mapping <= self.start && self.start < source_mapping_end {
             if self.end < source_mapping_end { // overflow at both end
-                map=Some(MapInterval::new_int(source.start+self.start-source.mapping, self.lenght, self.mapping));
-                overflow=Some(MapInterval::new_int(source.start+self.end-source.mapping, source_mapping_end-self.end, self.end));
+                map=Some(MapInterval::new(source.start+self.start-source.mapping, self.lenght, self.mapping));
+                overflow=Some(MapInterval::new(source.start+self.end-source.mapping, source_mapping_end-self.end, self.end));
             }
             else { // no overflow at end
-                map=Some(MapInterval::new_int(source.start+self.start-source.mapping, source_mapping_end-self.start, self.mapping));
+                map=Some(MapInterval::new(source.start+self.start-source.mapping, source_mapping_end-self.start, self.mapping));
             }
-            overflow2=Some(MapInterval::new_int(source.start, self.start-source.mapping, source.mapping));
+            overflow2=Some(MapInterval::new(source.start, self.start-source.mapping, source.mapping));
         }
         return (map,overflow, overflow2);
     }
@@ -129,31 +120,30 @@ fn first_part(content: &String) {
     let mut seeds: Vec<i64> = re.find_iter(seeds_line).map(|m| m.as_str().parse().unwrap_or(0)).collect();
     lines.next();
     lines.next();
-    let mut convertion: Vec<i64> = vec!();
     let mut maps: Vec<MapInterval> = vec!();
     for line in lines.into_iter() {
         if line.len() == 0 {
+            let mut mapped_seeds: Vec<i64> = vec!();
             for seed in seeds.iter() {
                 let mut matched = false;
                 for m in maps.iter() {
                     if m.contains(*seed) {
-                        convertion.push(m.map(*seed));
+                        mapped_seeds.push(m.map(*seed));
                         matched = true;
                         break;
                     }
                 }
                 if !matched {
-                    convertion.push(*seed);
+                    mapped_seeds.push(*seed);
                 }
             }
             maps.clear();
-            seeds = convertion.clone();
-            convertion.clear();
+            seeds = mapped_seeds;
             continue;
         }
         if line.contains("map") {continue;}
-        let interval = re.find_iter(line).map(|m| m.as_str().parse().unwrap_or(0)).collect();
-        maps.push(MapInterval::new(&interval))
+        let raw_map: Vec<i64> = re.find_iter(line).map(|m| m.as_str().parse().unwrap_or(0)).collect();
+        maps.push(MapInterval::new(raw_map[1], raw_map[2], raw_map[0]));
     }
 
     let total = seeds.iter().min().unwrap_or(&0);
